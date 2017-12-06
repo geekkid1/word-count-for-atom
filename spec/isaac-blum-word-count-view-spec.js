51f67d87e97ce9d53c8af78ea8f8dca8fae801bb
geekkid1 <ibtronfan@gmail.com>
2017-12-06 08:40:45 -0500

    "add initial files"

---
+++ spec/isaac-blum-word-count-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import IsaacBlumWordCountView from '../lib/isaac-blum-word-count-view';
+
+describe('IsaacBlumWordCountView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
